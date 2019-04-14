99302164932ba34d7930d57ea90d7ebc46cf9c45
digitalsoba <anthony.inlavong@gmail.com>
2019-04-14 00:24:43 -0700

    ?

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,17 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+protected function disableExceptionHandling()
+{
+    $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+    $this->app->instance(ExceptionHandler::class, new class extends Handler {
+        public function __construct() {}
+        public function report(\Exception $e) {}
+        public function render($request, \Exception $e) {
+            throw $e;
+        }
+    });
+}
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
