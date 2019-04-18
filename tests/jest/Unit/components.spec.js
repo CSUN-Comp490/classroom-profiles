7b85c79a56107dbe466c71fd6a98bdade36cbac8
digitalsoba <anthony.inlavong@gmail.com>
2019-04-17 23:37:10 -0700

    Add equipment vue test

--- tests/jest/Unit/components.spec.js
+++ tests/jest/Unit/components.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils'
-import welcome from '../../../resources/js/components/welcome.vue'
-import navbar from '../../../resources/js/components/topnavbar.vue'
+import welcome from '../../../resources/js/components/welcome'
+import navbar from '../../../resources/js/components/topnavbar'
+import equipment from '../../../resources/js/components/equipment'
 
 test('Does welcome.vue exist?', () => {
   var wrapper = mount(welcome)
@@ -11,3 +12,9 @@ test('Does navbar.vue exist?', () => {
   var wrapper = mount(navbar)
   expect(wrapper.isVisible()).toBe(true)
 })
+
+test('Does equipment.vue exist?', () => {
+  var wrapper = mount(equipment)
+  expect(wrapper.isVisible()).toBe(true)
+})
+
