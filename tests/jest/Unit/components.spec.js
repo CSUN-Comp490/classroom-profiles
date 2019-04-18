5dc32b0e49bba93ca484c0e5c0fde9210066ab29
digitalsoba <anthony.inlavong@gmail.com>
2019-04-17 23:58:32 -0700

    Revert to 7b85c79a56107dbe466c71fd6a98bdade36cbac8

--- tests/jest/Unit/components.spec.js
+++ tests/jest/Unit/components.spec.js
@@ -1,7 +1,6 @@
 import { mount } from '@vue/test-utils'
-import welcome from '../../../resources/js/components/welcome'
-import navbar from '../../../resources/js/components/topnavbar'
-import equipment from '../../../resources/js/components/equipment'
+import welcome from '../../../resources/js/components/welcome.vue'
+import navbar from '../../../resources/js/components/topnavbar.vue'
 
 test('Does welcome.vue exist?', () => {
   var wrapper = mount(welcome)
@@ -12,9 +11,3 @@ test('Does navbar.vue exist?', () => {
   var wrapper = mount(navbar)
   expect(wrapper.isVisible()).toBe(true)
 })
-
-test('Does equipment.vue exist?', () => {
-  var wrapper = mount(equipment)
-  expect(wrapper.isVisible()).toBe(true)
-})
-
